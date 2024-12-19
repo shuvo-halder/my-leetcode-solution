142642afaee1c3065966a00e4316fcfc1b2339b1
SHUVO HALDER <samshuvo00@gmail.com>
2024-12-19 22:58:06 +0600

    add two sum

--- Add two number/Soluition.py
+++ Add two number/Soluition.py
@@ -1,26 +1,33 @@
 import os, sys
 from typing import List, Tuple
 
+# input l1 = [2,4,3], l2 = [5,6,4]
+# input l1 = [0], l2 = [0]
+
+
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        temp = dummy
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def addTwoNumbers(self, l1, l2):
+        dummy_head = Solution(0)
+        current = dummy_head
         carry = 0
-        while (l1 != None or l2 != None) or carry:
-            sum = 0
-            if l1 != None:
-                sum += l1.val
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            carry = total // 10
+            current.next = Solution(total % 10)
+
+            current = current.next
+            if l1:
                 l1 = l1.next
-            if l2 != None:
-                sum = l2.val
+            if l2:
                 l2 = l2.next
 
-            sum += carry
-            carry = sum // 10
-            node = ListNode(sum % 10)
-            temp.next = node
-            temp = temp.next
-
-        return dummy.next
-
+        return dummy_head.next
 
