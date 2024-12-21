fef64bd6d71f525d52bc002af867568bdd4c7b4b
SHUVO HALDER <samshuvo00@gmail.com>
2024-12-21 22:39:38 +0600

    trying something

--- longest substring/main.py
+++ longest substring/main.py
@@ -1,6 +1,6 @@
 class Solution:
 
-    def lengthOfLongestSubstring(self, s):
+    def lengthOfLongestSubstring(self, charS: str):
         char_index_map = {}
         max_length = 0
         start = 0
@@ -10,6 +10,13 @@ class Solution:
                 start = char_index_map[char] + 1
             char_index_map[char] = i
             max_length = max(max_length, i - start + 1)
+            print(max_length)
         return max_length
+    
+
+    if __name__ == '__main__':
+        charS = input()
+        lengthOfLongestSubstring(charS)
+        # aprint(res)
 
         
